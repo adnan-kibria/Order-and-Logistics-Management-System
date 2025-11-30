37b8525d5ac47dd5fab75e3e8c4a529b7b12b721
adnan-kibria <adnankibria@yahoo.com>
2025-11-30 16:32:53 +0600

    Seleral route added without test
    Routes are assignDeliveryMan, confirmOrder, cancelOrder and
    getTotalSales

--- backend/src/orders/order.controller.ts
+++ backend/src/orders/order.controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Param, Patch, Post } from "@nestjs/common";
 import { Orders } from "./entities/orders.entity";
 import { OrderService } from "./order.service";
 
@@ -11,4 +11,34 @@ export class OrderController {
     // placeOrder(@Body()obj : any) : Promise<Orders>{
 
     // }
+    //kibria
+    @Patch('assign-deliveryman/:orderId/:deliveryManId')
+    assignDeliveryMan(
+        @Param('orderId') orderId: number,
+        @Param('deliveryManId', ) deliveryManId: number
+    ): Promise<Orders> {
+        return this.orderService.assignDeliveryMan(orderId, deliveryManId);
+    }
+
+    //kibria
+    @Patch('confirm-order/:orderId')
+    confirmOrder(
+        @Param('orderId') orderId: number,
+    ): Promise<Orders> {
+        return this.orderService.confirmOrder(orderId);
+    }
+    //kibria
+    @Patch('cancel-order/:orderId')
+    cancelOrder(
+        @Param('orderId') orderId: number,
+    ): Promise<Orders> {
+        return this.orderService.cancelOrder(orderId);
+    }
+    //kibria
+    @Post('total-sales/:filter')
+    getTotalSales(
+        @Param('filter') filter: string,
+    ): Promise<Orders> {
+        return this.orderService.getTotalSales(filter);
+    }
 }
