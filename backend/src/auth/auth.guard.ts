f7e44ba165b3277426fbfd599ff863eb37000d65
adnan-kibria <adnankibria@yahoo.com>
2025-12-02 03:04:07 +0600

    n

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -19,11 +19,9 @@ export class AuthGuard implements CanActivate {
             const payload = await this.jwtService.verifyAsync(
                 token,
                 {
-                    secret: jwtConstants.secret
+                    secret: process.env.JWT_SECRET,
                 }
             );
-            // 💡 We're assigning the payload to the request object here
-            // so that we can access it in our route handlers
             request['user'] = payload;
         } catch {
             throw new UnauthorizedException();
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,12 +5,17 @@ import { AuthService } from './auth.service';
 import { UserModule } from 'src/users/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from 'src/common/constants/constants';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule,
+  imports: [
+    ConfigModule.forRoot({
+        isGlobal: true,
+      }),
+    UserModule,
     JwtModule.register({
       global: true,
-      secret: jwtConstants.secret,
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '180s' }
     })],
   controllers: [AuthController],
