a36c87bf4518485766e2423fbc35b3463b4f5c28
adnan-kibria <adnankibria@yahoo.com>
2025-12-02 01:07:22 +0600

    import

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -1,36 +1,40 @@
-// /* eslint-disable @typescript-eslint/no-unsafe-return */
-// /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// /* eslint-disable prettier/prettier */
-// import { CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable prettier/prettier */
+import { CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+import { jwtConstants } from "../common/constants/constants";
 
 
-// export class AuthGuard implements CanActivate {
-//     async canActivate(context: ExecutionContext): Promise<boolean> {
-//         const request: Request = context.switchToHttp().getRequest();
-//         const token = this.extractTokenFromHeader(request);
-//         if (!token) {
-//             throw new UnauthorizedException();
-//         }
-//         // try {
-//         //     const payload = await this.jwtService.verifyAsync(
-//         //         token,
-//         //         {
-//         //             secret: jwtConstants.secret
-//         //         }
-//         //     );
-//         //     // 💡 We're assigning the payload to the request object here
-//         //     // so that we can access it in our route handlers
-//         //     request['user'] = payload;
-//         // } catch {
-//         //     throw new UnauthorizedException();
-//         // }
-//         return true;
+export class AuthGuard implements CanActivate {
+    constructor(private readonly jwtService: JwtService) {}
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request: Request = context.switchToHttp().getRequest();
+        const token = this.extractTokenFromHeader(request);
+        if (!token) {
+            throw new UnauthorizedException();
+        }
+        try {
+            const payload = await this.jwtService.verifyAsync(
+                token,
+                {
+                    secret: jwtConstants.secret
+                }
+            );
+            // 💡 We're assigning the payload to the request object here
+            // so that we can access it in our route handlers
+            request['user'] = payload;
+        } catch {
+            throw new UnauthorizedException();
+        }
+        return true;
 
-//     }
+    }
 
-//     private extractTokenFromHeader(request: Request): string | undefined {
-//         const [type, token] = request.headers['authorization']?.split(' ') ?? [];
-//         return type === 'Bearer' ? token : undefined;
-//     }
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+        return type === 'Bearer' ? token : undefined;
+    }
 
-// }
+}
