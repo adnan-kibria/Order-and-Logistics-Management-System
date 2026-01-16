3b8899830178a7cb23b4afce0de3eb2d5b1111f8
Shad38 <sadman.shad55@gmail.com>
2026-01-16 17:49:52 +0600

    category dto added & route & service logic added

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { Categories } from 'src/products/entities/categories.entity';
+import { CreateCategoryDto } from './dto/category.dto';
 
 @Controller('category')
 export class CategoryController {
@@ -12,4 +13,15 @@ export class CategoryController {
     async getAllCategories() : Promise<Categories[]> {
         return this.categoryService.getAllCategories();
     }
+
+    @Post('create-category')
+    async create(@Body() body: CreateCategoryDto) {
+        return this.categoryService.createCategory(body);
+    }
+
+    @Get(':id')
+    async findById(@Param('id') id: number) {
+    return this.categoryService.getCategoryByIdp(id);
+    }
+
 }
--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Categories } from 'src/products/entities/categories.entity';
 import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './dto/category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -10,5 +11,25 @@ export class CategoryService {
 
     getAllCategories(): Promise<Categories[]> {
         return this.categoryRepository.find();
+    }
+
+    getCategoryById(id: number) {
+      throw new Error('Method not implemented.');
+    }
+    searchCategory(name: string) {
+        throw new Error('Method not implemented.');
+    }  
+
+    async createCategory(dto: CreateCategoryDto) {
+        const category = this.categoryRepository.create(dto);
+        return this.categoryRepository.save(category);
+    }
+
+    async getCategoryByIdp(id: number): Promise<Categories> {
+    const category = await this.categoryRepository.findOne({ where: { id } });
+    if (!category) {
+        throw new NotFoundException('Category not found');
+    }
+    return category;
     }
 }
