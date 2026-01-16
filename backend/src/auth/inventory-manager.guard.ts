59b23ed95cf49e177a426aef33e9f3b56dd9d5da
Shad38 <sadman.shad55@gmail.com>
2026-01-16 18:15:42 +0600

    guard added for inventory manager

---
+++ backend/src/auth/inventory-manager.guard.ts
@@ -0,0 +1,42 @@
+
+/* eslint-disable no-debugger */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable prettier/prettier */
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+
+export interface JwtPayload {
+    sub: string;
+    role: string;
+    iat: number;
+    exp: number;
+}
+
+@Injectable()
+export class InventoryManagerGuard implements CanActivate {
+    constructor(private jwtService: JwtService) { }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request: Request = context.switchToHttp().getRequest();
+
+        const token: string = request.cookies['jwt'];
+        if (!token) {
+            throw new UnauthorizedException('No JWT token found');
+        }
+
+        try {
+            const data: JwtPayload = await this.jwtService.verifyAsync(token);
+
+            if (data.role === 'inventorymanager') {
+                return true; // allow access
+            } else {
+                throw new UnauthorizedException('Not an inventory manager');
+            }
+        } catch (err) {
+            throw new UnauthorizedException('Invalid token');
+        }
+
+    }
+}
